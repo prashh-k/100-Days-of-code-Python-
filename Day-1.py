32ba4f015c91c468127def891d781133e1e9d497
Prashant Kharche <104455228+prashh-k@users.noreply.github.com>
2023-07-29 15:40:07 +0530

    Add files via upload

---
+++ Day-1.py
@@ -0,0 +1,14 @@
+print("hello world")
+print("""Hi
+I am Prashant,
+And you?
+""")
+
+# Everything in the quotes ("") is text, also called string. 
+# Single set of double quote ("") only work  for a single line.
+# Triple set off double quote (""" """) work  for multiple line.
+
+print("""Prashant
+July 28th 2023
+""")  
+print("good bye")
