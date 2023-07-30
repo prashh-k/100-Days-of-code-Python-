884619d255b93c7f0b001c35389919baebaa6a85
Prashant Kharche <104455228+prashh-k@users.noreply.github.com>
2023-07-30 15:48:23 +0530

    Add files via upload

---
+++ Day-2-input_and_variable.py
@@ -0,0 +1,17 @@
+#Input 
+#Used to take input from user.
+myname = input("Enter Your Name: ")
+
+#variable 
+#Python Variable is containers that store values.
+#myName = input("Text :")
+#In above myname is a variable.
+
+a = input("What's your  favourite food: ")
+b = input("What's your  favourite music: ")
+c = input("Where do you live? : ")
+
+print(myname)
+print (a)
+print(b)
+print(c)
