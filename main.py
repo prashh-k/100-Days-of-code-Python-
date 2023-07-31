7eada6692a46f639afca2403f54af2ab60d39fa8
Prashant Kharche <104455228+prashh-k@users.noreply.github.com>
2023-07-31 23:59:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#concatenation
+# to join 2 string or variable in a single print statement 
+
+firstname = input("your firstname: ")
+lastname = input("your lastname: ")
+
+print("my name is ", firstname, lastname )
+
+
+# Challenge
+food = input("name a food:")
+plant = input("name a type of plant :")
+cook = input("name a method of cooking :")
+burned_food = input("what a word describes burned food:")
+DIY = input("name a DIY Item:")
+
+print("menu", \n , "saute ", food, "with ", burned_food, plant, "on a bed of ", DIY )
