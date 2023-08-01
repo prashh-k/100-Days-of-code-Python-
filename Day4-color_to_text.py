b0f8bf6a7acb3e05d8910da60ea4f3fc7250199f
Prashant Kharche <104455228+prashh-k@users.noreply.github.com>
2023-08-01 16:33:36 +0530

    Add files via upload

---
+++ Day4-color_to_text.py
@@ -0,0 +1,36 @@
+# Challenge
+# Advanture simulator
+# you will give your info through bunch of questions then we'll make you up an amazing story you as the star!
+
+# name = input("Your name is :")
+# enemy = input("Your worst enemy's name :")
+# superPower = input("your Superpower :")
+# print("Our story begins as our hero ", name,
+#       "approaches a forbidding castle...")
+# print("Suddenly a bolt of lightning striked the ground at the feet of", name)
+# print("Our final battle begins!' shout the evil ", enemy,
+#       "clearly missing the fact that", name, "has the power of", superPower,
+#       "which means they'll win quite easily")
+
+
+
+
+# changing color of text 
+# To change the color of text we should on the color code and then set it default later
+# If we won't set it to default it will keep printing all the text in that color
+
+# color            value
+# Dafault          0
+# Black            30
+# Red              31
+# Green            32
+# Yellow           33
+# Blue             34
+# Purple           35
+# cyan             36
+# White            37
+
+print("Uh, oh, you'r been given a", "\033[31m", "Warning",
+     "\033[0m", "for being a bad, bad person.")
+
+print("\n Prashant is a ", "\033[32m", "Good Person.","\033[0m", "Sid is a ", "\033[31m", "Bad Person.","\033[0m", "But both are", "\033[34m" "good friends.", "\033[0m")
