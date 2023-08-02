0b062ecbe17d68e1ec19ca3ac646f663bffb53f9
Prashant Kharche <104455228+prashh-k@users.noreply.github.com>
2023-08-02 23:55:35 +0530

    Add files via upload

---
+++ Day5-if_else.py
@@ -0,0 +1,30 @@
+#if else conditionals 
+
+# myName = input("What's your name: ")
+# if myName == "Prash" :
+#   print("Welcome !")
+#   print("Your a good man.")
+# else : 
+#   print("Your wrong dude,")
+#   print("GET OUT!!")
+
+# challenge 
+print("Marvel  Movie Character Creator \n --")
+spider = input("Do you like 'hanging aroung'?: ")
+if spider == "yes" :
+  print("Then yo're a spider.\n")
+else :
+  print("Then yo're a spider.\n")
+
+korg = input("Do you have 'gravelly' voice?: ")
+if spider == "yes" :
+  print("Then yo're a korg.\n")
+else :
+  print("Then yo're a korg.\n")
+
+captain = input("Do you often fell 'marvelous'?: ")
+if spider == "yes" :
+  print("Then yo're a Captain America.\n")
+else :
+  print("Then yo're a Captain America.\n")
+
